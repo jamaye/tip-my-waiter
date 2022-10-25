48560676f73bac00518fbb8c497173c50bb1b3c4
Janilou Sy <janilousy@yahoo.com>
2022-10-25 15:33:31 -0500

    functionality works

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var billAmount = 0.0;
+var bill = 0.0;
+var billWithTip = 0.0;
 var tipPercent = 0.0;
 var tip = 0.0;
 let inputValid = false;
@@ -6,48 +7,61 @@ var temp;
 
 
 //Bill section
-//Checks if the input is valid
+//Checks if the bill input is valid
 document.querySelector(".next-btn").addEventListener("click", function(){
   inputValid = false;
-  temp = document.getElementById("typeNumber").value;
-  if(checkValid(temp)){
-    console.log("inputValid:" + inputValid);    //
-    billAmount = temp;    //Stores value of the bill
+  if(checkValid()){
+    bill = temp;    //Stores value of the bill
     temp = document.getElementById("next");
     document.getElementsByClassName("form-label")[0].innerHTML = "Enter Amount"
     temp.href = "#service";
   }else{
-    console.log("inputValid:" + inputValid);   //
     temp = document.getElementById("next");
     document.getElementsByClassName("form-label")[0].innerHTML = "Please Enter Valid Amount"
     temp.href = "#bill";
   }
 });
 
-function checkValid(value){
-  var label ="";
+//Adds action listener to all buttons in Rate section
+for (let i = 0; i < 4; i++){
+  document.querySelectorAll("#rate-btn")[i].addEventListener("click", function(){
+    tipPercent = document.querySelectorAll(".btn")[i].value;
+    if(checkValid()){
+      this.href="#receipt";
+      addReceiptValue(inputValid)
+    }else{
+      this.href = "#bill";
+    }
+  });
+}
+
+//Rounds-up the bill
+document.getElementsByClassName("btn-roundup")[0].addEventListener("click", function(){
+  var text = document.getElementById("new-bill-amount");
+  text.innerHTML = Math.ceil(billWithTip);
+});
+
+//Function that sets inputValid that checks if bill is entered correctly
+function checkValid(){
+  temp = document.getElementById("typeNumber").value;
   if (temp > 0 && !temp.includes("e")){
     inputValid = true;
     return inputValid;
   } else{
-    document.getElementById("typeNumber").value = "";
     return inputValid;
   }
 }
 
-
-//Rating Section
-for (let i = 0; i < 4; i++){
-  document.querySelectorAll(".btn")[i].addEventListener("click", function(){
-    tipPercent = this.value;
-    checkToContinue(inputValid, i)
-  });
-}
-
-
-function checkToContinue(inputValid, i){
-  var text = document.getElementById("tip");
-  if(inputValid == true){
-    text.innerHTML = parseFloat(tipPercent*billAmount).toFixed(2);
+//Function that adds value to the receipt
+function addReceiptValue(inputValid, i){
+  var tipText = document.getElementById("tip");
+  var billText = document.getElementById("bill-amount");
+  var newBillText = document.getElementById("new-bill-amount");
+  if(checkValid){
+    tip = parseFloat(tipPercent*bill).toFixed(2);
+    billWithTip = (parseFloat(bill) + parseFloat(tip)).toFixed(2);
+    billText.innerHTML= bill;
+    tipText.innerHTML = tip;
+    newBillText.innerHTML = billWithTip;
   }
 }
